4e445e5546174079a91e98421179ab366dd9a4eb
DennisZ <yaoh888@gmail.com>
2025-11-11 11:24:38 +0800

    Fix formatting condition in formatZhDuration function to ensure proper output of minutes and seconds.

--- app.js
+++ app.js
@@ -816,7 +816,7 @@ function formatSeconds(total) {
 function formatZhDuration(seconds) {
 	const m = Math.floor(seconds / 60);
 	const s = Math.floor(seconds % 60);
-	//if (m > 0 && s > 0) return `${m}分${s}`;
+	if (m > 0 && s > 0) return `${m}分${s}`;
 	if (m > 0) return `${m}分`;
 	return `${s}`;
 }
